1ce5eb827a43be5fa515b398eb2bdbafa76f88d9
the-17th-fox <nikitos171002@yandex.by>
2022-07-05 14:25:27 +0300

    Added accounts policies and roles

---
+++ src/AccountsService/Constants/Auth/AccountsPolicies.cs
@@ -0,0 +1,14 @@
+﻿namespace AccountsService.Auth
+{
+    /// <summary>
+    /// Provides identity policies name. Should be used instead of direct role's-name specification
+    /// </summary>
+    public class AccountsPolicies
+    {
+        // Will provide access for all roles, such as admin, user, etc
+        public const string DefaultRights = "DefaultRights";    
+
+        // Will provide access for such roles as admin
+        public const string ElevatedRights = "ElevatedRights";  
+    }
+}
