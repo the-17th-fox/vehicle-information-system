dc833f4fa4faba5f47d9453ac855f041f8bad5c6
the-17th-fox <nikitos171002@yandex.by>
2022-07-05 14:26:20 +0300

    Added identity and policies parameters into startup

--- src/AccountsService/Program.cs
+++ src/AccountsService/Program.cs
@@ -1,4 +1,30 @@
+using AccountsService.Auth;
+using AccountsService.Infrastructure.Context;
+using AccountsService.Models;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Identity
+builder.Services.AddIdentity<User, IdentityRole<Guid>>(options =>
+{
+    options.Password.RequiredLength = 5;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireDigit = true;
+    options.User.RequireUniqueEmail = true;
+}).AddEntityFrameworkStores<AccountsServiceContext>();
+
+//Authorization
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy(AccountsPolicies.DefaultRights, policy =>
+        policy.RequireRole(AccountsRoles.DefaultUser, AccountsRoles.Administrator));
+
+    options.AddPolicy(AccountsPolicies.ElevatedRights, policy =>
+        policy.RequireRole(AccountsRoles.Administrator));
+});
 
 builder.Services.AddControllers();
 
