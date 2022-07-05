583312383f39af401413530631b3dd76ce575363
the-17th-fox <nikitos171002@yandex.by>
2022-07-05 14:18:44 +0300

    Added user model and DbContext

---
+++ src/AccountsService/Infrastructure/Context/AccountsServiceContext.cs
@@ -0,0 +1,46 @@
+﻿using AccountsService.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccountsService.Infrastructure.Context
+{
+    public class AccountsServiceContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    {
+        public AccountsServiceContext(DbContextOptions<AccountsServiceContext> opt) : base(opt) 
+        {
+            Database.EnsureCreated();
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker
+                .Entries()
+                .Where(e =>
+                    e.State == EntityState.Modified || e.State == EntityState.Added)
+                .Where(e =>
+                    e.Properties.Where(p =>
+                        p.Metadata.Name == "UpdatedAt" || p.Metadata.Name == "CreatedAt").Any());
+
+            if (!entries.Any())
+                return await base.SaveChangesAsync(cancellationToken);
+
+            foreach (var entityEntry in entries)
+            {
+                switch (entityEntry.State)
+                {
+                    case EntityState.Modified:
+                        entityEntry.Property("UpdatedAt").CurrentValue = DateTime.UtcNow;
+                        break;
+
+                    case EntityState.Added:
+                        entityEntry.Property("CreatedAt").CurrentValue = DateTime.UtcNow;
+                        break;
+                }
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
+}
---
+++ src/AccountsService/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AccountsService.Models
+{
+    public class User : IdentityUser<Guid>
+    {
+        public DateTime CreatedOn { get; private set; } = DateTime.UtcNow;
+        public DateTime LastUpdated { get; private set; } = DateTime.UtcNow;
+        public bool IsDeleted { get; set; }
+    }
+}
