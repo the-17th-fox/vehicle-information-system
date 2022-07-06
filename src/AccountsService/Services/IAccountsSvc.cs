9b14f2aeee5acacb55b69257f57ed6c9b8ea59c7
the-17th-fox <nikitos171002@yandex.by>
2022-07-06 15:05:07 +0300

    Add accounts service interface and its implementation

---
+++ src/AccountsService/Services/IAccountsSvc.cs
@@ -0,0 +1,11 @@
+﻿using AccountsService.Models;
+using AccountsService.Utilities;
+using AccountsService.ViewModels;
+
+namespace AccountsService.Services
+{
+    public interface IAccountsSvc
+    {
+        public Task RegisterAsync(User user, string password);
+    }
+}
