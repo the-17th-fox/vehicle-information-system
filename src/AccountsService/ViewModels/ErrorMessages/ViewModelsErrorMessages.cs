336297676e0eb55d374ab1a43304329e273714ca
the-17th-fox <nikitos171002@yandex.by>
2022-07-06 15:04:03 +0300

    Add registration view model and constant error messages

---
+++ src/AccountsService/ViewModels/ErrorMessages/ViewModelsErrorMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace AccountsService.Constants.ErrorMessages
+{
+    public class ViewModelsErrorMessages
+    {
+        public const string RequiredNotProvided = "Required field wasn't provided.";
+        public const string OutOfRange = "Provided value is out of range.";
+        public const string IncorrectEmail = "Provided email is incorrect.";
+    }
+}
---
+++ src/AccountsService/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using AccountsService.Constants.ErrorMessages;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace AccountsService.ViewModels
+{
+    public class RegistrationViewModel
+    {
+        [Required(ErrorMessage = ViewModelsErrorMessages.RequiredNotProvided)]
+        [StringLength(maximumLength: 15, MinimumLength = 3, ErrorMessage = ViewModelsErrorMessages.OutOfRange)]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage =ViewModelsErrorMessages.RequiredNotProvided)]
+        public string Password { get; set; } = string.Empty;
+        
+        [Required(ErrorMessage =ViewModelsErrorMessages.RequiredNotProvided)]
+        [EmailAddress(ErrorMessage = ViewModelsErrorMessages.IncorrectEmail)]
+        public string Email { get; set; } = string.Empty;
+    }
+}
