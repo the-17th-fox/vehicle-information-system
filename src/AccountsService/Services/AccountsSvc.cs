16cd25569e80bc2f7bd7c13f6020acba9addbff4
the-17th-fox <nikitos171002@yandex.by>
2022-07-06 16:16:18 +0300

    Added logging for service

--- src/AccountsService/Services/AccountsSvc.cs
+++ src/AccountsService/Services/AccountsSvc.cs
@@ -1,5 +1,7 @@
 ﻿using AccountsService.Constants.Auth;
+using AccountsService.Constants.Logger;
 using AccountsService.Models;
+using AccountsService.Utilities;
 using Microsoft.AspNetCore.Identity;
 using System.Reflection; // Isn't necessary after all exceptions will be implemented
 
@@ -8,21 +10,32 @@ namespace AccountsService.Services
     public class AccountsSvc : IAccountsSvc
     {
         private readonly UserManager<User> _userManager;
-        public AccountsSvc(UserManager<User> userManager) => _userManager = userManager;
+        private readonly ILogger<AccountsSvc> _logger;
+        public AccountsSvc(UserManager<User> userManager, ILogger<AccountsSvc> logger)
+        {
+            _userManager = userManager;
+            _logger = logger;
+        }
 
         public async Task RegisterAsync(User user, string password)
         {
             var result = await _userManager.CreateAsync(user, password);
+            _logger.LogInformation(LoggerHelper.LogUserActions(AccountLoggingActions.Registred, user));
+
             if(!result.Succeeded)
             {
                 // Should be replaced with custom exception (error handling middleware)
+                // Errors logging should be deligated to the error handling middleware
                 throw new NotImplementedException($"{MethodBase.GetCurrentMethod()!.Name} | {result.Errors.First().Description}");
             }
 
             result = await _userManager.AddToRoleAsync(user, AccountsRoles.DefaultUser);
+            _logger.LogInformation(LoggerHelper.LogUserActions(AccountLoggingActions.AddedToRole, user, AccountsRoles.DefaultUser));
+
             if(!result.Succeeded)
             {
                 // Should be replaced with custom exception (error handling middleware)
+                // Errors logging should be deligated to the error handling middleware
                 throw new NotImplementedException($"{MethodBase.GetCurrentMethod()!.Name} | {result.Errors.First().Description}");
             }
         }
