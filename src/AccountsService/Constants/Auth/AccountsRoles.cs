3dbc2e2e80dd186b87cf109bc09f37c7be1e40e5
the-17th-fox <nikitos171002@yandex.by>
2022-07-06 17:09:47 +0300

    Replaced unnecessary AccountsRoles method  with auto property

--- src/AccountsService/Constants/Auth/AccountsRoles.cs
+++ src/AccountsService/Constants/Auth/AccountsRoles.cs
@@ -7,11 +7,10 @@ namespace AccountsService.Constants.Auth
         public const string DefaultUser = "DefaultUser";
         public const string Administrator = "Administrator";
 
-        private static readonly List<IdentityRole<Guid>> Roles = new()
+        public static List<IdentityRole<Guid>> Roles { get; } = new()
         {
             new IdentityRole<Guid>(AccountsRoles.DefaultUser) { Id = Guid.NewGuid(), NormalizedName = AccountsRoles.DefaultUser.Normalize() },
             new IdentityRole<Guid>(AccountsRoles.Administrator) { Id = Guid.NewGuid(), NormalizedName = AccountsRoles.Administrator.Normalize() }
         };
-        public static List<IdentityRole<Guid>> GetRoles() => Roles;
     }
 }
