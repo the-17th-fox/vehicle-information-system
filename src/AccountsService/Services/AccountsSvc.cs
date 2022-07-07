cec184fbf045b3c68c0a0e4b872cd120cb450dc5
the-17th-fox <nikitos171002@yandex.by>
2022-07-07 17:50:52 +0300

    Added login methods to get claims and return jwt token from service to controller

--- src/AccountsService/Services/AccountsSvc.cs
+++ src/AccountsService/Services/AccountsSvc.cs
@@ -4,6 +4,10 @@ using AccountsService.Exceptions.CustomExceptions;
 using AccountsService.Models;
 using AccountsService.Utilities;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Reflection; // Isn't necessary after all exceptions will be implemented
+using System.Security.Claims;
 
 namespace AccountsService.Services
 {
@@ -15,6 +19,51 @@ namespace AccountsService.Services
         {
             _userManager = userManager;
             _logger = logger;
+        }
+
+        public List<Claim> GetClaims(User user, IList<string> userRoles)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            };
+
+            foreach (var role in userRoles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            return claims;
+        }
+
+        public JwtSecurityToken CreateSecurityToken(IOptions<JwtConfigugartionModel> securityConfig, List<Claim> claims)
+        {
+            return new(
+                issuer: securityConfig.Value.Issuer,
+                audience: securityConfig.Value.Audience,
+                notBefore: DateTime.UtcNow,
+                claims: claims,
+                expires: DateTime.UtcNow.AddHours(securityConfig.Value.LifetimeHours));
+        }
+
+        public async Task<string> LoginAsync(string email, string password, IOptions<JwtConfigugartionModel> securityConfig)
+        {
+            // todo: add logging to this method
+            var user = await _userManager.FindByEmailAsync(email);
+            if (user is null)
+            {
+                throw new NotImplementedException();
+            }
+
+            if (!await _userManager.CheckPasswordAsync(user, password))
+            {
+                throw new NotImplementedException();
+            }
+
+            var userClaims = GetClaims(user, await _userManager.GetRolesAsync(user));
+            var token = CreateSecurityToken(securityConfig, userClaims);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
         public async Task RegisterAsync(User user, string password)
--- src/AccountsService/Services/IAccountsSvc.cs
+++ src/AccountsService/Services/IAccountsSvc.cs
@@ -1,11 +1,17 @@
 ﻿using AccountsService.Models;
 using AccountsService.Utilities;
 using AccountsService.ViewModels;
+using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace AccountsService.Services
 {
     public interface IAccountsSvc
     {
         public Task RegisterAsync(User user, string password);
+        public Task<string> LoginAsync(string email, string password, IOptions<JwtConfigugartionModel> securityConfig);
+        protected List<Claim> GetClaims(User user, IList<string> userRoles);
+        protected JwtSecurityToken CreateSecurityToken(IOptions<JwtConfigugartionModel> securityConfig, List<Claim> claims);
     }
 }
