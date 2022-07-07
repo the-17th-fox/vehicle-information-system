f7e40c7718fccf1e9a73ec8bdc378707e53d7063
the-17th-fox <nikitos171002@yandex.by>
2022-07-07 17:49:41 +0300

    Added endpoint for user logins

---
+++ src/AccountsService/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using AccountsService.ViewModels.ErrorMessages;
+using System.ComponentModel.DataAnnotations;
+
+namespace AccountsService.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = ViewModelsErrorMessages.RequiredNotProvided)]
+        public string Password { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = ViewModelsErrorMessages.RequiredNotProvided)]
+        [EmailAddress(ErrorMessage = ViewModelsErrorMessages.IncorrectEmail)]
+        public string Email { get; set; } = string.Empty;
+    }
+}
