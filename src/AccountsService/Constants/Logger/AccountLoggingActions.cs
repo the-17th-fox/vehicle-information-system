7be463bef6f6393ceb072f9f4cfb63fdd6211cde
the-17th-fox <nikitos171002@yandex.by>
2022-07-07 15:42:45 +0300

    Replaced AccountLoggingActions with more wide LoggingForm

--- src/AccountsService/Constants/Logger/AccountLoggingActions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AccountsService.Constants.Logger
-{
-    public class AccountLoggingActions
-    {
-        public const string Registred = "Registration";
-        public const string AddedToRole = "AddedToRole";
-    }
-}
