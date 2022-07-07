b8244cb06621b2a69db123a30a97edfe366ffade
the-17th-fox <nikitos171002@yandex.by>
2022-07-07 17:48:38 +0300

    Added JWT configuration

---
+++ src/AccountsService/Utilities/JwtConfigugartionModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AccountsService.Utilities
+{
+    public class JwtConfigugartionModel
+    {
+        public string Issuer { get; set; } = string.Empty;
+        public string Audience { get; set; } = string.Empty;
+        public string Key { get; set; } = string.Empty;
+        public double LifetimeHours { get; set; } = 1D;
+    }
+}
