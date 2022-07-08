8f8747bc9d10a6fcfd96944ce47670d2150d19bb
the-17th-fox <nikitos171002@yandex.by>
2022-07-08 15:19:46 +0300

    Added logging to Login method

--- src/AccountsService/Constants/Logger/LoggingForms.cs
+++ src/AccountsService/Constants/Logger/LoggingForms.cs
@@ -4,6 +4,7 @@
     {
         public const string Registred = "User {username}:{email} has successfully registred.";
         public const string AddedToRole = "User {username}:{email} has successfully added to role {role}";
+        public const string LoggedIn = "User {username}:{email} has logged in as - {roles}";
         public const string ExceptionForm = "{exception} has occured with status code {statusCode}: {message}";
     }
 }
--- src/AccountsService/Services/AccountsSvc.cs
+++ src/AccountsService/Services/AccountsSvc.cs
@@ -6,7 +6,6 @@ using AccountsService.Utilities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using System.IdentityModel.Tokens.Jwt;
-using System.Reflection; // Isn't necessary after all exceptions will be implemented
 using System.Security.Claims;
 
 namespace AccountsService.Services
@@ -48,65 +47,22 @@ namespace AccountsService.Services
 
         public async Task<string> LoginAsync(string email, string password, IOptions<JwtConfigugartionModel> securityConfig)
         {
-            // todo: add logging to this method
             var user = await _userManager.FindByEmailAsync(email);
             if (user is null)
             {
-                throw new NotImplementedException();
+                throw new NotFoundException($"User with provided email was not found {email}") ;
             }
 
             if (!await _userManager.CheckPasswordAsync(user, password))
             {
-                throw new NotImplementedException();
+                throw new UnauthorizedException("Invalid credentials");
             }
 
-            var userClaims = GetClaims(user, await _userManager.GetRolesAsync(user));
+            var userRoles = await _userManager.GetRolesAsync(user);
+            var userClaims = GetClaims(user, userRoles);
             var token = CreateSecurityToken(securityConfig, userClaims);
 
-            return new JwtSecurityTokenHandler().WriteToken(token);
-        }
-
-        public List<Claim> GetClaims(User user, IList<string> userRoles)
-        {
-            var claims = new List<Claim>()
-            {
-                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-            };
-
-            foreach (var role in userRoles)
-            {
-                claims.Add(new Claim(ClaimTypes.Role, role));
-            }
-
-            return claims;
-        }
-
-        public JwtSecurityToken CreateSecurityToken(IOptions<JwtConfigugartionModel> securityConfig, List<Claim> claims)
-        {
-            return new(
-                issuer: securityConfig.Value.Issuer,
-                audience: securityConfig.Value.Audience,
-                notBefore: DateTime.UtcNow,
-                claims: claims,
-                expires: DateTime.UtcNow.AddHours(securityConfig.Value.LifetimeHours));
-        }
-
-        public async Task<string> LoginAsync(string email, string password, IOptions<JwtConfigugartionModel> securityConfig)
-        {
-            // todo: add logging to this method
-            var user = await _userManager.FindByEmailAsync(email);
-            if (user is null)
-            {
-                throw new NotImplementedException();
-            }
-
-            if (!await _userManager.CheckPasswordAsync(user, password))
-            {
-                throw new NotImplementedException();
-            }
-
-            var userClaims = GetClaims(user, await _userManager.GetRolesAsync(user));
-            var token = CreateSecurityToken(securityConfig, userClaims);
+            _logger.LogInformation(LoggingForms.LoggedIn, user.UserName, user.Email, string.Join(' ', userRoles));
 
             return new JwtSecurityTokenHandler().WriteToken(token);
         }
@@ -116,7 +72,7 @@ namespace AccountsService.Services
             var result = await _userManager.CreateAsync(user, password);
             if(!result.Succeeded)
             {
-                throw new InvalidParamsException($"{result.Errors.First<IdentityError>().Description}");
+                throw new InvalidParamsException(result.Errors.First<IdentityError>().Description);
             }
             _logger.LogInformation(LoggingForms.Registred, user.UserName, user.Email);
 
@@ -124,7 +80,7 @@ namespace AccountsService.Services
             result = await _userManager.AddToRoleAsync(user, AccountsRoles.DefaultUser);
             if(!result.Succeeded)
             {
-                throw new Exception($"{result.Errors.First<IdentityError>().Description}");
+                throw new Exception(result.Errors.First<IdentityError>().Description);
             }
             _logger.LogInformation(LoggingForms.AddedToRole, user.UserName, user.Email, AccountsRoles.DefaultUser);
         }
