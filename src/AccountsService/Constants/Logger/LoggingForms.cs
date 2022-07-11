dabcb0fc647fcf4e42ac521df25dfbc4e2585fad
the-17th-fox <nikitos171002@yandex.by>
2022-07-11 14:40:31 +0300

    Added several logging forms

--- src/AccountsService/Constants/Logger/LoggingForms.cs
+++ src/AccountsService/Constants/Logger/LoggingForms.cs
@@ -2,9 +2,19 @@
 {
     public class LoggingForms
     {
-        public const string Registred = "User {username}:{email} has successfully registred.";
-        public const string AddedToRole = "User {username}:{email} has successfully added to role {role}";
-        public const string LoggedIn = "User {username}:{email} has logged in as - {roles}";
-        public const string ExceptionForm = "{exception} has occured with status code {statusCode}: {message}";
+        public const string RegistrationAttempt = "The user [{username}]:[{email}] is trying to register";
+        public const string Registred = "The user [{username}]:[{email}] was successfully registred";
+        public const string FailedToRegister = "The user [{username}]:[{email}] has failed the registration: [{error}]";
+    
+        public const string LoginAttempt = "The user [{}] is trying to log in";
+        public const string LoggedIn = "The user [{email}] was successfully logged in";
+
+        public const string AddedToRole = "The user [{username}]:[{email}] was successfully added to role {role}";
+
+
+        public const string UserNotFound = "The user [{email}] was not found";  
+        public const string InvalidCredentials = "The user [{email}] has failed authentication";
+
+        public const string ExceptionForm = "[{exception}] has occured with status code [{statusCode}]: [{message}]";
     }
 }
