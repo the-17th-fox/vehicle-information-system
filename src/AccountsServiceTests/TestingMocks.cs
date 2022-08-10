f960452352ee042823c2cbe116e64d3517f6fd74
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2022-08-10 17:43:04 +0300

    Moved mocks to its own separated class

---
+++ src/AccountsServiceTests/TestingMocks.cs
@@ -0,0 +1,42 @@
+﻿using AccountsService.Infrastructure.Context;
+using AccountsService.Models;
+using AccountsService.Services;
+using AccountsService.Utilities;
+using AccountsServiceTests.TestingData;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountsServiceTests.Mocks
+{
+    internal class TestingMocks
+    {
+        internal Mock<UserManager<User>> UserManager { get; set; } = null!;
+        internal Mock<ILogger<AccountsSvc>> Logger { get; set; } = null!;
+        internal AccountsServiceContext Context { get; set; } = null!;
+        internal IOptions<JwtConfigurationModel> JwtConfig { get; set; } = null!;
+
+        internal TestingMocks()
+        {
+            Logger = new();
+            UserManager = new Mock<UserManager<User>>(Mock.Of<IUserStore<User>>(), null, null, null, null, null, null, null, null);
+
+            var options = new DbContextOptionsBuilder<AccountsServiceContext>()
+                .UseInMemoryDatabase(databaseName: "AccountsDb")
+                .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+                .Options;
+
+            Context = new(options);
+
+            JwtConfig = SampleData.GetSampleJwtOptions();
+        }
+    }
+}
