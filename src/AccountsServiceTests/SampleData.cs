31bc549710ece05af74db92e8fedcbfd039a2977
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2022-08-10 17:44:11 +0300

    Added Login tests

--- src/AccountsServiceTests/SampleData.cs
+++ src/AccountsServiceTests/SampleData.cs
@@ -1,11 +1,15 @@
-﻿using AccountsService.Models;
+﻿using AccountsService.Constants.Auth;
+using AccountsService.Models;
+using AccountsService.Utilities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AccountsServiceTests
+namespace AccountsServiceTests.TestingData
 {
     internal class SampleData
     {
@@ -19,6 +23,22 @@ namespace AccountsServiceTests
                 Email = "SampleUserEmail@mail.com",
                 IsDeleted = isDeleted,
             };
+        }
+
+        public static IOptions<JwtConfigurationModel> GetSampleJwtOptions()
+        {
+            return Options.Create(new JwtConfigurationModel()
+            {
+                Key = "ThisIsASampleJsonWebTokenSecurityKey",
+                Audience = "SampleAudience",
+                Issuer = "SampleIssuer",
+                LifetimeHours = 1
+            });
+        }
+
+        public static IList<string> GetSampleDefaultUserRoles()
+        {
+            return new List<string>() { AccountsRoles.DefaultUser };
         }
     }
 }
---
+++ src/AccountsServiceTests/Tests/LoginTests.cs
@@ -0,0 +1,106 @@
+﻿using AccountsService.Exceptions.CustomExceptions;
+using AccountsService.Models;
+using AccountsService.Services;
+using AccountsServiceTests.Mocks;
+using AccountsServiceTests.TestingData;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AccountsServiceTests.Tests
+{
+    public class LoginTests
+    {
+        private readonly TestingMocks _mocks = new();
+
+        [Fact]
+        public async void Login_UserNotExists_NotFoundException()
+        {
+            // Arrange
+            User user = null!;
+
+            _mocks.UserManager.Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
+                .ReturnsAsync(user)
+                .Verifiable();
+
+            var service = new AccountsSvc(_mocks.UserManager.Object, _mocks.Logger.Object, _mocks.Context);
+
+            // Act & Assert
+            await Assert.ThrowsAsync<NotFoundException>(() => service.LoginAsync(It.IsAny<string>(), It.IsAny<string>(), null!))
+                .WaitAsync(CancellationToken.None);
+            _mocks.UserManager.VerifyAll();
+        }
+
+        [Fact]
+        public async void Login_UserIsDeleted_NotFoundException()
+        {
+            // Arrange
+            var user = SampleData.GetSampleUser(isDeleted: true);
+
+            _mocks.UserManager.Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
+                .ReturnsAsync(user)
+                .Verifiable();
+
+            var service = new AccountsSvc(_mocks.UserManager.Object, _mocks.Logger.Object, _mocks.Context);
+
+            // Act & Assert
+            await Assert.ThrowsAsync<NotFoundException>(() => service.LoginAsync(It.IsAny<string>(), It.IsAny<string>(), null!))
+                .WaitAsync(CancellationToken.None);
+            _mocks.UserManager.VerifyAll();
+        }
+
+        [Fact]
+        public async void Login_WrongPassword_UnauthorizedException()
+        {
+            // Arrange
+            var user = SampleData.GetSampleUser(isDeleted: false);
+
+            _mocks.UserManager.Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
+                .ReturnsAsync(user)
+                .Verifiable();
+
+            _mocks.UserManager.Setup(x => x.CheckPasswordAsync(user, It.IsAny<string>()))
+                .ReturnsAsync(false)
+                .Verifiable();
+
+            var service = new AccountsSvc(_mocks.UserManager.Object, _mocks.Logger.Object, _mocks.Context);
+
+            // Act & Assert
+            await Assert.ThrowsAsync<UnauthorizedException>(() => service.LoginAsync(It.IsAny<string>(), It.IsAny<string>(), _mocks.JwtConfig))
+                .WaitAsync(CancellationToken.None);
+            _mocks.UserManager.VerifyAll();
+        }
+
+        [Fact]
+        public async void Login_LoginSucceded_StringWithJWT()
+        {
+            // Arrange
+            var user = SampleData.GetSampleUser(isDeleted: false);
+
+            _mocks.UserManager.Setup(x => x.FindByEmailAsync(It.IsAny<string>()))
+                .ReturnsAsync(user)
+                .Verifiable();
+
+            _mocks.UserManager.Setup(x => x.CheckPasswordAsync(user, It.IsAny<string>()))
+                .ReturnsAsync(true)
+                .Verifiable();
+
+            _mocks.UserManager.Setup(x => x.GetRolesAsync(user))
+                .ReturnsAsync(SampleData.GetSampleDefaultUserRoles);
+
+            var service = new AccountsSvc(_mocks.UserManager.Object, _mocks.Logger.Object, _mocks.Context);
+
+            // Act
+            var result = await service.LoginAsync(It.IsAny<string>(), It.IsAny<string>(), _mocks.JwtConfig);
+
+            // Assert
+            _mocks.UserManager.VerifyAll();
+            Assert.False(string.IsNullOrWhiteSpace(result));
+        }
+    }
+}
