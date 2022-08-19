94ce837cc5daa134fd1e48589a03cc51854131b2
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2022-08-19 22:27:49 +0300

    Added mongo database connection features

---
+++ src/AccountsService/Utilities/LogsContextConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace AccountsService.Utilities
+{
+    public class LogsContextConfiguration
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string CollectionName { get; set; } = string.Empty;
+    }
+}
