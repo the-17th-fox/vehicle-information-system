370e9990e809f52a15d293b38fec9582bf2cb260
the-17th-fox <nikitos171002@yandex.by>
2022-08-31 18:36:48 +0300

    Added models and config

---
+++ src/VehiclesSearchService/Models/Manufacturer.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+namespace VehiclesSearchService.Models
+{
+    public class Manufacturer
+    {
+        [JsonProperty(PropertyName = "Mfr_ID")]
+        public int MfrId { get; set; }
+
+        [JsonProperty(PropertyName = "Mfr_Name")]
+        public string MfrFullName { get; set; } = string.Empty;
+
+        [JsonProperty(PropertyName = "Mfr_CommonName")]
+        public string MfrCommonName { get; set; } = string.Empty;
+
+        public string Country { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+
+        [JsonProperty(PropertyName = "StateProvince")]
+        public string State { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string ContactEmail { get; set; } = string.Empty;
+        public List<VehicleType> VehicleTypes { get; set; } = new();
+    }
+
+    public class VehicleType
+    {
+        [JsonProperty(PropertyName = "GVWRFrom")]
+        public string WeightRatingFrom { get; set; } = string.Empty;
+        [JsonProperty(PropertyName = "GVWRTo")]
+        public string WeightRatingTo { get; set; } = string.Empty;
+        public bool IsPrimary { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ src/VehiclesSearchService/ViewModels/ManufacturerSearchViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace VehiclesSearchService.ViewModels
+{
+    public class ManufacturerSearchViewModel
+    {
+        public int? MfrId { get; set; } = null;
+        public string MfrName { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+    }
+}
