0ff03c67ae71d4a9b50b2ba14425b6d8a523ef32
the-17th-fox <nikitos171002@yandex.by>
2022-08-31 18:36:13 +0300

    Added vehicles search microservice base

---
+++ src/VehiclesSearchService/Program.cs
@@ -0,0 +1,39 @@
+using Serilog;
+using VehiclesSearchService.Exceptions;
+using VehiclesSearchService.Services;
+using VehiclesSearchService.Utilities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IVehiclesSearchSvc, VehiclesSearchSvc>();
+builder.Services.Configure<NhtsaApiConfig>(builder.Configuration.GetSection("NHTSA"));
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog((context, config) =>
+{
+    config.ReadFrom.Configuration(context.Configuration);
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseMiddleware<GlobalExceptionsHandler>();
+
+app.MapControllers();
+
+Log.Information("Vehicles Search Service successfully started");
+
+app.Run();
