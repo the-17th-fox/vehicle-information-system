3a6d7438f32521244ae7c268be06b166339088a2
the-17th-fox <nikitos171002@yandex.by>
2022-09-09 18:06:52 +0300

    Added class for getting filled properties of an object

---
+++ src/Common/Utilities/PropertyLookup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Utilities
+{
+    public class PropertyLookup
+    {
+        public static IEnumerable<PropertyInfo> GetFilledProperties(object obj)
+        {
+            return obj.GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .Where(p => p.CanRead)
+                .Select(p => new { Property = p, Value = p.GetValue(obj, null) })
+                .Where(a => a.Value != null && !string.IsNullOrEmpty(a?.Value?.ToString()))
+                .Select(a => a.Property);
+        }
+    }
+}
