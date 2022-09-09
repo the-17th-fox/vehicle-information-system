0fe2f81e9133b60b865da394379479b8409f39e3
the-17th-fox <nikitos171002@yandex.by>
2022-09-09 18:08:01 +0300

    Added proper search by every parameter from the search model

--- src/VehiclesSearchService/Controllers/ManufacturersController.cs
+++ src/VehiclesSearchService/Controllers/ManufacturersController.cs
@@ -1,6 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Common.CustomExceptions;
+using Common.Utilities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.Options;
+using System;
+using System.Data.SqlTypes;
+using System.IO.IsolatedStorage;
+using System.Net;
 using System.Net.Http.Headers;
 using VehiclesSearchService.Models;
 using VehiclesSearchService.Services;
@@ -9,6 +16,15 @@ using VehiclesSearchService.ViewModels;
 
 namespace VehiclesSearchService.Controllers
 {
+    /// <summary>
+    /// :: TODO :: 
+    /// - Add logging
+    /// - Add caching using Redis
+    /// - Add auth
+    /// - Refactor methods (may methods should moved somewhere?)
+    /// - Add pagination
+    /// </summary>
+
     [Route("api/search/manufacturers")]
     [ApiController]
     public class ManufacturersController : ControllerBase
@@ -17,7 +33,7 @@ namespace VehiclesSearchService.Controllers
         private readonly HttpClient _httpClient;
         private IOptions<NhtsaApiConfig> _nhtsaApiConfig;
         private readonly IVehiclesSearchSvc _searchSvc;
-        private string _apiUrl { get => _nhtsaApiConfig.Value.ApiUrl; }
+        private string _responseFormat = @"?format=json";
 
         public ManufacturersController(
             ILogger<VehiclesController> logger,
@@ -26,40 +42,129 @@ namespace VehiclesSearchService.Controllers
         {
             _logger = logger;
             _nhtsaApiConfig = nhtsaApiConfig;
+            
             _httpClient = new();
             _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(@"application/json"));
+            _httpClient.BaseAddress = new Uri($@"{_nhtsaApiConfig.Value.ApiUrl}/");
+            
             _searchSvc = searchSvc;
         }
 
-        private async Task<string> GetResponse(string uri)
+        private async Task<string> GetResponseAsync(string path)
         {
-            _httpClient.BaseAddress = new Uri(uri);
-
-            var response = await _httpClient.GetAsync(_httpClient.BaseAddress);
+            var response = await _httpClient.GetAsync(path);
 
             return await response.Content.ReadAsStringAsync();
         }
 
-
-        // Problem :: Users still have to provide mfrId or mfrName
-        // Goal :: Users should be able to find manufacturers by any parameter they want
-        [HttpGet]
-        public async Task<IActionResult> GetMfrInfo([FromQuery] ManufacturerSearchViewModel searchCriteria)
+        /// <summary>
+        /// Searchs for the first non-empty/non-null property
+        /// </summary>
+        /// <param name="searchCriteria"></param>
+        /// <returns>Endpoint uri</returns>
+        private string GetFirstAppropriateProperty(MfrSearchViewModel searchCriteria)
         {
-            if (searchCriteria.MfrId is null && string.IsNullOrWhiteSpace(searchCriteria.MfrName))
-                throw new Exception();
+            var filledProperties = PropertyLookup.GetFilledProperties(searchCriteria);
 
-            var requestParam = searchCriteria.MfrId is null ? searchCriteria.MfrName : searchCriteria.MfrId.ToString();
+            if (!filledProperties.Any())
+                throw new InvalidParamsException();
 
-            string mfrDetails = await GetResponse(@$"{_apiUrl}/getmanufacturerdetails/{requestParam}?format=json");
+            var containsNameOrId = filledProperties.Where(p =>
+                (p.Name == nameof(MfrSearchViewModel.MfrName) ||
+                p.Name == nameof(MfrSearchViewModel.MfrId)));
 
-            List<Manufacturer>? result;
-            if ((result = _searchSvc.ProvideManufacturersCheckout(mfrDetails, searchCriteria)).Count == 0)
+            // At first we're looking for MfrId or MfrName, because searching using these params are more efficient
+            if (containsNameOrId.Any())
+                return containsNameOrId.First().Name;
+
+            switch (filledProperties.First().Name)
             {
-                return NotFound();
+                case nameof(MfrSearchViewModel.Country):
+                    return nameof(MfrSearchViewModel.Country);
+
+                case nameof(MfrSearchViewModel.City):
+                    return nameof(MfrSearchViewModel.City);
+
+                default:
+                    throw new InvalidParamsException();
+            }
+        }
+
+        private async Task<List<DetailedManufacturer>> GetDetailedManufacturers(string property, MfrSearchViewModel searchCriteria)
+        {
+            switch (property)
+            {
+                case nameof(MfrSearchViewModel.MfrName):
+                    return await GetByIdentifiersAsync(searchCriteria.MfrName, searchCriteria);
+
+                case nameof(MfrSearchViewModel.MfrId):
+                    return await GetByIdentifiersAsync(searchCriteria!.MfrId!.ToString()!, searchCriteria);
+
+                case nameof(MfrSearchViewModel.Country):
+                case nameof(MfrSearchViewModel.City):
+                case nameof(MfrSearchViewModel.State):
+                    return await GetBySecondaryCreteriasAsync(searchCriteria);
+
+                default:
+                    throw new InvalidParamsException();
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetManufacturerInfoAsync([FromQuery] MfrSearchViewModel searchCriteria)
+        {
+            var appropriateProp = GetFirstAppropriateProperty(searchCriteria);
+
+            List<DetailedManufacturer> manufacturers = await GetDetailedManufacturers(appropriateProp, searchCriteria);
+
+            return Ok(manufacturers);
+        }
+
+        private async Task<List<DetailedManufacturer>> GetByIdentifiersAsync(string identifier, MfrSearchViewModel searchCreteria)
+        {
+            string uri = $@"GetManufacturerDetails/{identifier}{_responseFormat}";
+            var response = await GetResponseAsync(uri);
+
+            var filteredResult = _searchSvc.GetFilteredMfrs(response, searchCreteria);
+
+            return filteredResult;
+        }
+
+        private async Task<List<DetailedManufacturer>> GetBySecondaryCreteriasAsync(MfrSearchViewModel searchCreteria)
+        {
+            string uri = $@"GetAllManufacturers{_responseFormat}";
+
+            var manufacturers = QueueAllMfrsAsync(uri);
+
+            List<DetailedManufacturer> filteredManufacturers = new();
+
+            await foreach (var mfrs in manufacturers)
+            foreach (var mfr in mfrs)
+            {
+                var newFilteredMfrs = await GetByIdentifiersAsync(mfr.MfrId.ToString(), searchCreteria);
+                if(newFilteredMfrs.Any())
+                    filteredManufacturers.AddRange(newFilteredMfrs);
             }
 
-            return Ok(result);
+            return filteredManufacturers;
+        }
+
+        private async IAsyncEnumerable<List<BaseManufacturer>> QueueAllMfrsAsync(string uri)
+        {
+            int counter = 1;
+            bool isLastEmpty = false;
+            do
+            {
+                string response = await GetResponseAsync($"{uri}&page={counter++}");
+
+                var newMfrs = MfrDetailsResponse<BaseManufacturer, BaseVehicleType>.TryParseResponse(response)?.Results;
+
+                if (newMfrs is null || newMfrs.Count == 0)
+                    isLastEmpty = true;
+                else
+                    yield return newMfrs;
+
+            } while (!isLastEmpty && counter < 3);
         }
     }
 }
